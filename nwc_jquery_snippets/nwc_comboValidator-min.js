b9f3ab80045f2f5514aa3c3ed41e2d05b6c5acd3
Prem Ananth <prem.uideveloper@gmail.com>
2012-10-30 19:12:36 +0530

    updated

--- nwc_jquery_snippets/nwc_comboValidator-min.js
+++ nwc_jquery_snippets/nwc_comboValidator-min.js
@@ -1,10 +1,5 @@
 // JavaScript Document
 // This snippet contribution is from http://www.newwebcode.com
-// author:Prem Ananth. C; prem@newwebcode.com / prem.uideveloper@gmail.com
+// author:Prem Ananth. C;
 // licence:OpenSource
-
-//elementId: Pass the ID of the input element
-//mandatory: boolean value true/false to test if it is a mandatory field
-//placeHolderValue: This gives the placeholder value for version lower than html5. This attribute is available in HTML5
-//labelValue: Error message to be shown
 var nwc_comboValidator=function(a,b,c,d){var e=false;$("#"+a).val(c).css("color","#999");$("#"+a).parents("form").submit(function(){if(!e&&b){$(".errMsg").append(d+" is a required field.<br />");return false}});$("#"+a).bind("click focus",function(){if(!e)$(this).val("")});$("#"+a).blur(function(){if($(this).val()==""){$(this).val(c).css("color","#999");e=false}else{$(this).css("color","#000");e=true}})}
