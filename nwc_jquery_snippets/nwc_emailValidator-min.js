572b7e9ece167f7c43be174719280cc1b2e24919
Prem Ananth <prem.uideveloper@gmail.com>
2012-11-27 11:35:17 +0530

    js min update

--- nwc_jquery_snippets/nwc_emailValidator-min.js
+++ nwc_jquery_snippets/nwc_emailValidator-min.js
@@ -2,6 +2,4 @@
 // This snippet contribution is from http://www.newwebcode.com
 // author:Prem Ananth. C;
 // licence:OpenSource
-var nwc_emailValidator=function(inputName,errMessage)
-{emailPattern=/^([\w-\.]+@([\w-]+\.)+[\w-]{2,3})?$/;$('input[name="'+inputName+'"]').blur(function(){$('input[name="'+inputName+'"]').next('.invalidEmailErrMsg').hide();if(!emailPattern.test($('input[name="'+inputName+'"]').val()))
-$('input[name="'+inputName+'"]').after('<span class="invalidEmailErrMsg">'+errMessage+'</span>');});}
+var nwc_emailValidator=function(e,t){emailPattern=/^([\w-\.]+@([\w-]+\.)+[\w-]{2,3})?$/;$('input[name="'+e+'"]').blur(function(){$('input[name="'+e+'"]').next(".invalidEmailErrMsg").hide();if(!emailPattern.test($('input[name="'+e+'"]').val()))$('input[name="'+e+'"]').after('<span class="invalidEmailErrMsg">'+t+"</span>")})}
